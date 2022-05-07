73ecb9dfe947a89f375d586df5a7f0ceecedb560
Henrique Silva <hpsilva+git@pm.me>
2022-05-07 12:52:17 -0300

    Fix keyboard movement

--- YamiTestProject-PRG/Assets/Project/Scripts/KeyboardMovement.cs
+++ YamiTestProject-PRG/Assets/Project/Scripts/KeyboardMovement.cs
@@ -16,7 +16,7 @@ public class KeyboardMovement : MonoBehaviour
     {
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
         {
-            transform.position += new Vector3(10 * Time.deltaTime, 0, 0);
+            transform.position += new Vector3(0, 10 * Time.deltaTime, 0);
         }
         if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.A))
         {
@@ -28,7 +28,7 @@ public class KeyboardMovement : MonoBehaviour
         }
         if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3(0, 10 * Time.deltaTime, 0);
+            transform.position += new Vector3(10 * Time.deltaTime, 0, 0);
         }
     }
 
