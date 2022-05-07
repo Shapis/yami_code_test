229e83af58f8f6b4ca5a9d2dea950a973158e0d6
Henrique Silva <hpsilva+git@pm.me>
2022-05-07 13:08:51 -0300

    Fix mouse movement

--- YamiTestProject-PRG/Assets/Project/Scripts/MouseMovement.cs
+++ YamiTestProject-PRG/Assets/Project/Scripts/MouseMovement.cs
@@ -4,12 +4,33 @@ using UnityEngine;
 
 public class MouseMovement : MonoBehaviour
 {
+    private Coroutine _moveCouroutine;
     void Update()
     {
-        if(Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonUp(0))
         {
             Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            transform.position = mousePosition;
+
+            if (_moveCouroutine is not null)
+            {
+                StopCoroutine(_moveCouroutine);
+            }
+            _moveCouroutine = StartCoroutine(MoveOverTime(this.gameObject, mousePosition));
+        }
+    }
+
+    /// <summary>
+    /// Receives a GameObject and moves it to the targetPosition over the durationInSeconds
+    /// </summary>
+    IEnumerator MoveOverTime(GameObject gameObject, Vector3 targetPosition, float durationInSeconds = 1f)
+    {
+        float timer = 0f;
+        Vector3 initialPosition = gameObject.transform.position;
+        while (gameObject.transform.position != targetPosition)
+        {
+            gameObject.transform.position = Vector3.Lerp(initialPosition, targetPosition, timer);
+            timer += Time.deltaTime / durationInSeconds;
+            yield return null;
         }
     }
 }
