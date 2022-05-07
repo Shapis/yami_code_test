07be383e68f43a82d803147c0a347374148d70c5
Henrique Silva <hpsilva+git@pm.me>
2022-05-07 12:47:58 -0300

    Initial commit

---
+++ YamiTestProject-PRG/Assets/Project/Scripts/KeyboardMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(10 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-10 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.S))
+        {
+            transform.position += new Vector3(0, -10 * Time.deltaTime, 0);
+        }
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(0, 10 * Time.deltaTime, 0);
+        }
+    }
+
+}
---
+++ YamiTestProject-PRG/Assets/Project/Scripts/MouseMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.position = mousePosition;
+        }
+    }
+}
